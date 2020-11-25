b8c01ecbfec7daeb69001b766d25d1c979b026cb
Choic <caiciyong@vip.qq.com>
2020-11-25 16:54:45 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,6 @@
+# 使用github acitons 定时执行api
+Settings-Secrets里面添加`REQUEST_URLS`
+##
+`REQUEST_URLS`:需要定时执行的api,多个用分号`;`隔开
+
+
---
+++ request.js
@@ -0,0 +1,21 @@
+var request = require('request-promise');
+let URLS = process.env.REQUEST_URLS.split(';');
+console.log(URLS);
+URLS.map((v,i,a)=>{
+    setTimeout(()=>{
+        request(v).then(res=>{
+            res = JSON.parse(res);
+            console.log(res.code);
+            if(res.code==200){
+                console.log(v,'上车成功');
+            }else if(res.code==400){
+                console.log(v,'已经上车过了');
+            }else{
+                console.log(v,'上车失败');
+            }
+        }).catch(err=>{
+            console.log(v,'上车失败');
+            console.log(err)
+        })
+    },i+2000)
+})
